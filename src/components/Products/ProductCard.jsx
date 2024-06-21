ce0cc4136c34218238cbf788ec42f2b471ac3f62
Xuan Thang <thangijr@gmail.com>
2024-06-21 22:07:38 +0700

    update

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,36 +1,74 @@
 import { Link } from "react-router-dom";
 import Button from "../Shared/Button";
-
+import Slider from "react-slick";
 const ProductCard = ({ data }) => {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <div className="mb-10">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 place-items-center">
-        {/* Cart section */}
-        {data.map((item, index) => (
-          <div className="group" key={index}>
-            <div className="relative ">
-              <img
-                src={item.image}
-                alt=""
-                className="h-[180px] w-[260px] object-cover rounded-md "
-              />
-              {/* hover button */}
-              <Link
-                to={`/product-detail/${item._id}`}
-                className=" hidden group-hover:flex absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-full w-full text-center group-hover:backdrop-blur-sm justify-center items-center duration-200"
-              >
-                <button
-                  className={` cursor-pointer hover:scale-105 duration-300 py-2 px-8 rounded-full relative z-10 text-white `}
-                >
-                  Chi tiết
-                </button>
-              </Link>
-            </div>
-            <div className="leading-7">
-              <h2 className="font-bold">{item.name}</h2>
-            </div>
-          </div>
-        ))}
+      <div className=" gap-5 place-items-center">
+        <div className="slider-container mx-auto text-center">
+          <Slider {...settings}>
+            {/* Cart section */}
+            {data.map((item, index) => (
+              <div className="group  gap-5" key={index}>
+                <div className="relative ">
+                  <img
+                    src={item.image}
+                    alt=""
+                    className=" mx-auto h-[180px] w-[260px] object-cover rounded-md "
+                  />
+                  {/* hover button */}
+                  <Link
+                    to={`/product-detail/${item._id}`}
+                    className="hidden group-hover:flex absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-full w-full text-center group-hover:backdrop-blur-sm justify-center items-center duration-200"
+                  >
+                    <button
+                      className={` cursor-pointer hover:scale-105 duration-300 py-2 px-8 rounded-full relative z-10 text-white text-lg`}
+                    >
+                      Chi tiết
+                    </button>
+                  </Link>
+                </div>
+                <div className="leading-7">
+                  <h2 className="font-bold">{item.name}</h2>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
       </div>
       {/* <div className="text-center my-4">
         <button className="border-2 border-sky-500 font-light p-2 rounded-xl w-[200px] hover:bg-sky-500 hover:text-white">
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,14 +3,14 @@ import Heading from "../Shared/Heading";
 import ProductCard from "./ProductCard";
 
 const Products = ({ products }) => {
-  const displayedProducts = products.slice(0, 4);
+  // const displayedProducts = products.slice(0, 4);
   return (
     <div>
       <div data-aos="fade-up" className="container mt-2">
         {/* Header section */}
         <Heading title="Our Products" subtitle="Explore Our Products" />
         {/* Body section */}
-        <ProductCard data={displayedProducts} />
+        <ProductCard data={products} />
       </div>
     </div>
   );
